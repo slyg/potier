34fe31f3e175267662c1f66fb2c790145b085e1f
Sylvain Faucherand <syl.faucherand@gmail.com>
2016-01-09 12:46:36 +0000

    Remove jquery dependency, use fetch, sorry ie users

--- app/scripts/shop/webApi.js
+++ app/scripts/shop/webApi.js
@@ -1,9 +1,9 @@
-import request from 'jquery';
-
 export function getBooks () {
-  return request.getJSON('/books');
+  return fetch('/books')
+    .then(response => response.json());
 };
 
 export function getBestOffer (price, isbns) {
-  return request.getJSON(`/books/bestoffer/${price}/${isbns.join(',')}`);
+  return fetch(`/books/bestoffer/${price}/${isbns.join(',')}`)
+    .then(response => response.json());
 };
